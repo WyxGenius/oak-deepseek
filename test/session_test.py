4313189d4395bc6e2c71c40f8337345b54d3629a
WyxGenius <2389604361@qq.com>
2026-03-28 11:45:44 +0800

    refactor:进行了一些重命名

--- test/session_test.py
+++ test/session_test.py
@@ -18,8 +18,8 @@ def mul(a: int, b: int) -> str:
     """
     return f"{a * b}"
 
-session: AgentEngine = AgentEngine()
-session.create_agent(
+engine: AgentEngine = AgentEngine()
+engine.create_agent(
     key=("sys","sys"),
     description="一个能进行数学计算的agent",
     prompt="你是一个数学计算助手，会使用工具确保自己计算的正确性。",
@@ -28,20 +28,20 @@ session.create_agent(
     sub_agents=[("sys","sub")]
 )
 
-session.create_agent(
+engine.create_agent(
     key=("sys","sub"),
     description="能进行乘法计算",
     prompt="你是一个数学计算助手，会使用工具确保自己计算的正确性",
     tools=[mul]
 )
 history_queue = Queue()
-session.init_core(key=("sys", "sys"), history_queue=history_queue, api_key=os.getenv("DEEPSEEK_API_KEY"))
+engine.init_core(key=("sys", "sys"), history_queue=history_queue, api_key=os.getenv("DEEPSEEK_API_KEY"))
 
 task_queue = Queue()
 task_queue.put("计算(679+678)*(2+78)")
 
 def go():
-    session.run(task_queue)
+    engine.run(task_queue)
 
 def messages():
     while True:
