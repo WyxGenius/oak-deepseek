f901876ac6e4c0a236b17c04f7f0705d11699e0e
WyxGenius <2389604361@qq.com>
2026-03-30 15:55:10 +0800

    docs:版本从0.0.56更新为0.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('../../'))  # 让 Sphinx 能找到 oak_deepse
 project = 'Oak DeepSeek'
 copyright = '2026, WyxGenius'
 author = 'WyxGenius'
-release = '0.0.56'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
